5a555d39801f76ee4442de8c4f10b3ffdc7a1c86
Abega1642 <hei.abega.2@gmail.com>
2025-10-27 19:20:35 +0300

    feat: add ffmpeg dependency

--- src/main/java/dev/razafindratelo/arsmedia/endpoint/rest/controller/MediaUploadController.java
+++ src/main/java/dev/razafindratelo/arsmedia/endpoint/rest/controller/MediaUploadController.java
@@ -1,4 +1,10 @@
 package dev.razafindratelo.arsmedia.endpoint.rest.controller;
 
-public class MediaUploadController {
-}
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/media")
+public class MediaUploadController {}
--- src/main/java/dev/razafindratelo/arsmedia/service/MediaService.java
+++ src/main/java/dev/razafindratelo/arsmedia/service/MediaService.java
@@ -1,3 +1,31 @@
 package dev.razafindratelo.arsmedia.service;
 
-public class MediaService {}
+import dev.razafindratelo.arsmedia.file.BucketComponent;
+import dev.razafindratelo.arsmedia.model.Audio;
+import dev.razafindratelo.arsmedia.model.Image;
+import dev.razafindratelo.arsmedia.model.Video;
+import jakarta.transaction.NotSupportedException;
+import jakarta.validation.constraints.NotNull;
+import java.io.File;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
+@AllArgsConstructor
+public class MediaService {
+  private final BucketComponent bucket;
+
+  public Video uploadVideo(@NotNull File video) throws NotSupportedException {
+    throw new NotSupportedException("NOT IMPLEMENTED");
+  }
+
+  public Audio uploadAudio(@NotNull Audio audio) throws NotSupportedException {
+    throw new NotSupportedException("NOT IMPLEMENTED");
+  }
+
+  public Image uploadImage(@NotNull Image image) throws NotSupportedException {
+    throw new NotSupportedException("NOT IMPLEMENTED");
+  }
+}
