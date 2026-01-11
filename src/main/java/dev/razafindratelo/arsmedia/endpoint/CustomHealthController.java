a36bb8bb81d327f59dbb227d223cdabb3ac59100
unknown <sololeveling20@users.noreply.github.com>
2026-01-12 03:59:50 +0530

    Rename HealthEndpoint to avoid actuator conflict

--- src/main/java/dev/razafindratelo/arsmedia/endpoint/CustomHealthController.java
+++ src/main/java/dev/razafindratelo/arsmedia/endpoint/CustomHealthController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthEndpoint {
+public class CustomHealthController {
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "ok";
+    @GetMapping("/custom-health")
+    public String health() {
+        return "OK";
     }
 }
